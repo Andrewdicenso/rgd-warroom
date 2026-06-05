14c04f7ade07a5a42fb6ee4c4e98579e75c2e8b6
Andrewdicenso <andrewdicenso@libero.it>
2026-06-05 20:30:08 +0300

    ddssd

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ from core.notifier import Sentinella
 
 # Importiamo la logica centralizzata dal pacchetto auth
 from auth.auth import inizializza_sessione, login_utente, logout_utente
-from core.simulator import SimulatoreRischio
-from core.visuals import genera_grafico_predittivo
+#from core.simulator import SimulatoreRischio
+#from core.visuals import genera_grafico_predittivo
 # =========================
 #   CONFIGURAZIONE BASE
 # =========================
