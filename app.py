b8225994979185cce701474320b99a1dc6f825bc
Andrewdicenso <andrewdicenso@libero.it>
2026-06-06 18:23:13 +0300

    app.py

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ from core.notifier import Sentinella
 
 # Importiamo la logica centralizzata dal pacchetto auth
 from auth.auth import inizializza_sessione, login_utente, logout_utente
-from core import SimulatoreRischio
-#from core.visuals import genera_grafico_predittivo
+sys.path.append("core")
+from core.simulator import SimulatoreRischio
+from core.visuals import genera_grafico_predittivo
 # =========================
 #   CONFIGURAZIONE BASE
 # =========================
