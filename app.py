dbcb243f9c5faed00f938680b28349d2a90eb07a
Andrewdicenso <andrewdicenso@libero.it>
2026-06-18 15:09:20 +0300

    logout

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import plotly.express as px
 import streamlit as st
 
 # 3. MODULI CORE & AUTH (I tuoi file locali)
-from auth.auth import inizializza_sessione, login_utente, logout
+from auth.auth import inizializza_sessione, login_utente, logout_utente
 from core.database import DatabaseAziendale
 from core.engine import DataGateway
 from core.ingestor import IngestoreDati
