7df0caa34f8eec88bf3fdd5c1fadb0a5d6d8e0b6
Andrewdicenso <andrewdicenso@libero.it>
2026-06-24 14:08:48 +0300

    data referimg

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from auth.auth import inizializza_sessione, login_utente, logout_utente
 from core.database import DatabaseAziendale
 from core.engine import DataGateway
 from core.ingestor import IngestoreDati
-from core.refinery import DataRefinery
+from data_refinery import DataRefinery
 
 
 # --- RISOLUZIONE DINAMICA PATH INTERNI ---
