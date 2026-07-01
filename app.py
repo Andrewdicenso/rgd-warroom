1c90c8e50d1373d53f41ab1c2bdcf43560125096
Andrewdicenso <andrewdicenso@libero.it>
2026-07-01 23:38:50 +0300

    porca la madonna zoccola di merda

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ from dotenv import load_dotenv
 from core.ingestor import IngestoreDati
 from core.engine import DataGateway
 from core.database import DatabaseAziendale
-from consulente import ConsulenteAziendale
-
+from connectors.consulente import ConsulenteAziendale
 # Importiamo la logica centralizzata dal pacchetto auth
 from auth.auth import inizializza_sessione, login_utente, logout_utente
 
